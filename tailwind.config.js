06b6342e530259254fc84a519c7893817d9c8f1b
vanegasvan1 <vanegasvan1@gmail.com>
2026-08-15 01:32:22 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,14 @@ module.exports = {
         sans: ['Inter', 'sans-serif'],
       },
       colors: {
-        // Tono cálido, suave y elegante (Terracota/Arcilla apagado) 
-        // Esto le dará color al botón de votar y de ingresar, pero sin ser agresivo a la vista.
         brand: {
-          50: '#fdf8f6',
-          100: '#f2e8e5',
-          400: '#d4a392',
-          500: '#b87a62', // Color principal suave
-          600: '#9c634f', // Color para el efecto hover
-          700: '#7a4b3a',
-          800: '#5c382a',
-          900: '#40251c',
-        },
-        // Un tono dorado opaco y sutil para suavizar el logo de la pizza y las estrellas
-        amber: {
-          400: '#d4b483', 
+          50: '#fff7ed',
+          100: '#ffedd5',
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+          800: '#9a3412',
+          900: '#7c2d12',
         }
       }
     }
