e2d6af0993f6b2bf6907b224553c4b669c5ab4ad
vanegasvan1 <vanegasvan1@gmail.com>
2026-08-15 01:52:15 -0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,13 @@ module.exports = {
       },
       colors: {
         brand: {
-          50: '#fdf8f8',
-          100: '#F4D8D8', // Color 5 (Blanco grisáceo/rosado)
-          200: '#d9c0bf',
-          300: '#BEA8A7', // Color 4 (Gris claro)
-          400: '#c0a09e', 
-          500: '#C09891', // Color 3 (Color principal para botones)
-          600: '#9b746a', // Tono intermedio calculado para el hover
-          700: '#775144', // Color 2 (Marrón/Gris medio)
-          800: '#502c22',
-          900: '#2A0800', // Color 1 (Negro/Marrón muy oscuro)
+          50: '#fff7ed',
+          100: '#ffedd5',
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+          800: '#9a3412',
+          900: '#7c2d12',
         }
       }
     }
